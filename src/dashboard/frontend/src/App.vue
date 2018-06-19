da161715a4afc9a9e201a1d877cf3ec7e61b6e47
alufers <alufers@wp.pl>
2018-06-19 16:02:49 +0200

    Prepared frontend boilerplate

---
+++ src/dashboard/frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+ <h1>Siema</h1>
+</template>
+
+<script lang="ts">
+import Component from 'vue-class-component'
+import Vue from 'vue'
+
+@Component({
+  components: {
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+
+</style>
